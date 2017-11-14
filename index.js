eca714462416dc1f3b44858fb83c4a9b8f3beb4e
David L Bean <david.l.bean@mac.com>
2017-11-13 21:17:28 -0700

    refactored to use GITHUB_USERNAME env var in s3 names

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ let sharp = require('sharp');
 
 // init AWS
 let s3 = new AWS.S3();
-const source_bucket_name = 'cs4540-fall-2017-images-originals';
-const target_bucket_name = 'cs4540-fall-2017-images-sized';
+const github_username = process.env.GITHUB_USERNAME;
+const source_bucket_name = 'cs4540-fall-2017-' + github_username + '-originals';
+const target_bucket_name = 'cs4540-fall-2017-' + github_username + '-sized';
 const resolutions = [[50, 50], [100,100], [500,500]];
 
 // retrieve an original image from S3 (must already exist!)  Image is
